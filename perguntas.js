8d076a0cefcfe2fa7afdfc6b167992006368eda9
douglas252 <127516959+douglas252@users.noreply.github.com>
2024-09-02 08:20:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+export const perguntas = [
+    {
+        enunciado: " Como as ações sociais podem impactar positivamente a comunidade e quais são as principais desafios enfrentados dessas iniciativas?"
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "Superar esses desafios requer planejamento estratégico, colaboração entre diferentes atores (governo, ONGs, setor privado e comunidade) e uma abordagem adaptável e sensível às realidades locais.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+   
+        ]
+    }
+];
